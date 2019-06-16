10a8a08cc9226b6e7e05284d971588692161b445
znimed <znimed@gmail.com>
2019-06-16 19:38:50 +0000

    Initial commit

---
+++ database_setup.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Categories(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class CategoriesItem(Base):
+    __tablename__ = 'categories_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    categories_id = Column(Integer, ForeignKey('categories.id'))
+    categories = relationship(Categories)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+
+
+engine = create_engine('sqlite:///categoriesitems.db')
+
+
+Base.metadata.create_all(engine)
